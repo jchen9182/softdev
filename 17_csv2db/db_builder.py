8ea63ca04c286292ffd7e1ab072b87a8fc8f0bc9
jchen9182 <jchen9182@gmail.com>
2019-10-10 17:45:08 -0400

    fixed bug

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,7 +1,7 @@
 # Tyler Huang and Justin Chen
 # SoftDev1 pd02
 # K#17 -- No Trouble
-# 2019-10-08
+# 2019-10-10
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -15,26 +15,24 @@ command = "CREATE TABLE courses (code TEXT,  mark INTEGER, id INTEGER);" # creat
 c.execute(command)    # run SQL statement
 
 with open('courses.csv', 'r') as csvfile: # open csv file
-    reader = csv.DictReader(csvfile, delimiter = ",") 
+    reader = csv.DictReader(csvfile, delimiter = ",")
     #convert csv into dictionary with , as delimiter
-    reader.__next__() #skip first line
     for line in reader:
-        command = "INSERT INTO courses VALUES (\"" + line['code'] + "\"," + line['mark'] + "," + line['id'] + ");" 
-        # insert code, mark, id into table 
-        c.execute(command) 
+        command = "INSERT INTO courses VALUES (\"" + line['code'] + "\"," + line['mark'] + "," + line['id'] + ");"
+        # insert code, mark, id into table
+        c.execute(command)
         #print(line['code'])
 
 # Repeat the same process but with students.csv
-command = "CREATE TABLE students (name TEXT,  age INTEGER, id INTEGER);" 
-c.execute(command)    
+command = "CREATE TABLE students (name TEXT,  age INTEGER, id INTEGER);"
+c.execute(command)
 
-with open('students.csv', 'r') as csvfile: 
-    reader = csv.DictReader(csvfile, delimiter = ",") 
-    reader.__next__()
+with open('students.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter = ",")
     for line in reader:
-        command = "INSERT INTO students VALUES (\"" + line['name'] + "\"," + line['age'] + "," + line['id'] + ");" 
+        command = "INSERT INTO students VALUES (\"" + line['name'] + "\"," + line['age'] + "," + line['id'] + ");"
         # insert name, age, id into table
-        c.execute(command) 
+        c.execute(command)
 
 db.commit() #save changes
 db.close()  #close database
