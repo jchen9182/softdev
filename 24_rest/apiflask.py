caf9402bcafad93dfe98786904f48ad0af5faf2b
jchen9182 <jchen9182@gmail.com>
2019-11-12 19:35:36 -0500

    finished

--- 24_rest/apiflask.py
+++ 24_rest/apiflask.py
@@ -4,12 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    link = 'https://api.nasa.gov/planetary/apod?api_key=OLk8CUnTmRg7KfoKMh17Uva6gzWkkfA9IjaCb4ub'
-    picLink = 'https://apod.nasa.gov/apod/image/1911/NGC3717_Hubble_1080.jpg'
     http = urllib3.PoolManager()
-    response = http.request('GET', link)
-    json.loads(response.data)
-    return render_template('index.html')
+    link = 'https://api.nasa.gov/planetary/apod?api_key=OLk8CUnTmRg7KfoKMh17Uva6gzWkkfA9IjaCb4ub'
+    req = http.request('GET', link)
+    data = json.loads(req.data)
+    return render_template('index.html', pic = data['url'], text = data['explanation'])
 
 if __name__ == '__main__':
     app.debug = True
