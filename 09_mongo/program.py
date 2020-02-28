38f01185c9b8c9ec87bc717e51551a9ef23b457d
jchen9182 <jchen9182@gmail.com>
2020-02-27 21:45:24 -0500

    changes

--- 09_mongo/program.py
+++ 09_mongo/program.py
@@ -1,12 +1,21 @@
-import pymango
+from pymongo import MongoClient
+import json
 
 FILENAME = "primer-dataset.json"
-
 file = open(FILENAME, "r")
 data = file.readlines()
 
-# for line in data:
-#     print(line)
+client = MongoClient('localhost', 27017) 
+db = client.test
+restaurants = db.restaurants
 
+for line in data:
+    restaurants.insert(line)
+
+# restaurants.delete_many({})
+
+# with open("primer-dataset.json", 'r') as file:
+#     dataset = json.load(file)
+# restaurants.insert_many(dataset)
 
 file.close()
