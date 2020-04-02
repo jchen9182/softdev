7be26c2c7783c685611222a7bba30b0dd291fe08
jchen9182 <jchen9182@gmail.com>
2020-04-01 20:54:37 -0400

    done

--- 15_svg/3/events.js
+++ 15_svg/3/events.js
@@ -7,8 +7,8 @@ var table = document.getElementsByTagName('table')[0];
 
 var clicky = function(e) {
   alert( this.innerHTML );
-  //Q: What will happen when next line is uncommented?
-  //e.stopPropagation();
+  // Q: What will happen when next line is uncommented?
+  // e.stopPropagation();
 };
 
 for (var x=0; x < tds.length; x++) {
